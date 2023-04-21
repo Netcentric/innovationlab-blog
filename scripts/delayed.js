076bf471cd66fc1f4aa177a7dafcb434cc5426f9
Marko Donke <marko.donke@netcentric.biz>
2023-04-21 13:27:48 +0200

    fix formatting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,26 +5,26 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 function injectScript(src) {
-    window.scriptsLoaded = window.scriptsLoaded || [];
-  
-    if (window.scriptsLoaded.indexOf(src)) {
-      const head = document.querySelector('head');
-      const script = document.createElement('script');
-  
-      script.src = src;
-      script.setAttribute('async', 'true');
-      head.append(script);
-      window.scriptsLoaded.push(src);
-    }
+  window.scriptsLoaded = window.scriptsLoaded || [];
+
+  if (window.scriptsLoaded.indexOf(src)) {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+
+    script.src = src;
+    script.setAttribute('async', 'true');
+    head.append(script);
+    window.scriptsLoaded.push(src);
   }
+}
 
 function loadAdobeDatacollection() {
-    window.adobeDataLayer = window.adobeDataLayer || [];
-  
-    const src = window.location.host === 'newhorizons-blog.innovationlab.cx'
-      ? 'https://assets.adobedtm.com/2d725b839720/84e64346d33d/launch-96e515074f06.min.js'
-      : 'https://assets.adobedtm.com/2d725b839720/84e64346d33d/launch-87d8e5f3357c-staging.min.js';
-    injectScript(src);
-  }
+  window.adobeDataLayer = window.adobeDataLayer || [];
 
-  loadAdobeDatacollection();
+  const src = window.location.host === 'newhorizons-blog.innovationlab.cx'
+    ? 'https://assets.adobedtm.com/2d725b839720/84e64346d33d/launch-96e515074f06.min.js'
+    : 'https://assets.adobedtm.com/2d725b839720/84e64346d33d/launch-87d8e5f3357c-staging.min.js';
+  injectScript(src);
+}
+
+loadAdobeDatacollection();
